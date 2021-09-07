34518ee69b2225fe847539ec93c37e3014047c25
Daniyal Zakir <41327126+Daniyalzakir321@users.noreply.github.com>
2021-09-08 00:50:51 +0500

    GatsBy-Starter

---
+++ src/pages/home.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from "gatsby"
+import Header from "../components/header"
+import Footer from "../components/footer"
+
+import { StaticImage } from "gatsby-plugin-image"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+export default function home() {
+    return (
+        <Layout>
+            <Header siteTitle={'Home'} />
+            <Seo title="Home" />
+            <StaticImage
+                src="../images/gatsby-astronaut.png"
+                width={200}
+                height={200}
+                quality={95}
+                formats={["auto", "webp", "avif"]}
+                alt="A Gatsby astronaut"
+                style={{ marginBottom: `1.45rem` }}
+            />
+            <Link to="/home">Home</Link><br />
+            <Link to="/about">About</Link>
+            <Footer />
+        </Layout>
+    )
+}
